ab8e666d2b90d7e0eece4e05cade6e01afea3484
Koora <108467957+Josu2003@users.noreply.github.com>
2026-09-20 11:07:13 +0200

    LovecL ARZ v2.2_autoupdate.lua

--- LovecL ARZ v2.2_autoupdate.lua
+++ LovecL ARZ v2.2_autoupdate.lua
@@ -3,7 +3,7 @@ script_name = "LovecL ARZ v2.2"
 script_author = "Koora"
 script_version = "2.2"
 
-local update_json_url = "https://raw.githubusercontent.com/Josu2003/LovecL_ARZ/refs/heads/main/update.json?token=GHSAT0AAAAAAEJTGSXWF3VGIYRETP7HBFBE2VPTLXQ"
+local update_json_url = "https://raw.githubusercontent.com/Josu2003/LovecL_ARZ/refs/heads/main/update.json"
 local updateVersion = ""
 local updateChangelog = ""
 local updateUrl = ""
